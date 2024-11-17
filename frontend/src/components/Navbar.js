1bc68dca03dd8c830638eff40a59313dee472e89
Zhu Weiji <zhuweiji1997@gmail.com>
2024-11-18 01:58:35 +0800

    changes

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -17,7 +17,12 @@ const Navbar = () => {
                 <div className="flex items-center justify-between h-16">
                     <div className="flex items-center">
                         <div className="flex-shrink-0">
-                            <span className="text-white font-bold text-xl">Journal App</span>
+                            <Link
+                                    to="/"
+                                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                                >
+                                <span className="text-white font-bold text-xl">Journal App</span>
+                            </Link>
                         </div>
                         <div className="">
                             <div className="ml-10 flex items-baseline space-x-4">
